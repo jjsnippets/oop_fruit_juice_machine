305fb61ff7f5e79ebf94bf481ce209a68caea408
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2024-09-26 23:08:36 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("test");
+
+        for (int x = 0; x < 10; x++){
+            System.out.println(x);
+        }
     }
 
     public static void showSelection() {
