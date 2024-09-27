83926b27b90e372cc40e9ddedd773c39e4b11fc4
Pat Aloba <clarquealoba442@gmail.com>
2024-09-27 18:33:54 +0800

    It this good? or nah? i need checking

--- src/DispenserType.java
+++ src/DispenserType.java
@@ -3,32 +3,57 @@ public class DispenserType {
     private int cost;
 
     public DispenserType() {
-        // code here
+        numberOfItems = 0;
+        cost = 0;
     }
 
     public DispenserType(int setNoOfItems, int setCost) {
-        // code here
+        if (setNoOfItems >= 0){
+            numberOfItems = setNoOfItems;
+        } else {
+            numberOfItems = 0; // no negatives
+        }
+        if (setCost >= 0) {
+            cost = setCost;
+        } else {
+            cost = 0; // no negatives
+        }
     }
 
     public void makeSale() {
-        // code here
+        if (numberOfItems > 0){
+            numberOfItems--;
+        } else {
+            System.out.println("sold out"); 
+        }
     }
 
     public int getNoOfItems() {
-        // code here
-        return 0;
+        return this.numberOfItems;
     }
 
     public int getCost() {
-        // code here
-        return 0;
+        return this.cost;
     }
 
     public void makeSale(int amount) {
-        // code here
+        if (numberOfItems > 0){
+            if (amount >= cost) {
+                numberOfItems--;
+                int change = amount - cost;
+                System.out.println("Here drink, here change " + change);
+            } else {
+                System.out.println("kuwang, tag " + cost + "mani");
+            }
+        } else {
+            System.out.println("sold out");   
+        }
     }
 
     public static void displayProducts(DispenserType[] products) {
-        // code here
+        System.out.print("Products");
+        for (int i = 0; i < products.length; i++){
+            System.out.println("Product " + (i + 1) + ": " + products[i].getNoOfItems() + "items, cost " + products[i].getCost())
+        }
     }
 }
